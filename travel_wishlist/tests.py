92c2a125c8d0e632d4561a867fb6f67002f4105e
Brendon <brennon.mckeever@gmail.com>
2017-02-23 16:43:22 -0600

    added some tests

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -5,11 +5,27 @@ from .models import Place
 
 class TestViewHomePageIsEmptyList(TestCase):
 
-    """docstring for ."""
-
 
     def test_load_home_page_shows_empty_list(self):
 
         response = self.client.get(reverse('place_list'))
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
-        self.assertFalse
+        self.assertFalse(response.context['places']) # Empty lists are False
+
+class TestWishList(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_view_wishlist(self):
+
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        # What was sent to the template?
+        data_rendered = list(response.context['places'])
+
+        # What is in the database? And get all that equals False
+        data_expected = list(Place.objects.filter(visited = False))
+
+        # Did we get what we want?
+        self.assertCountEqual(data_rendered, data_expected)
